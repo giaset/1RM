fbd962deae79f73fc2676ca36e888f8624e4995a
Gianni Settino <g@miltonandparc.com>
2016-02-17 16:23:32 -0500

    Initial setup

---
+++ 1RM/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  1RM
+//
+//  Created by Gianni Settino on 2016-02-17.
+//  Copyright © 2016 Milton and Parc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.redColor()
+    }
+}
